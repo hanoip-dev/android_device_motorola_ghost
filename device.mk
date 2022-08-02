36d319c5c0d7345fbdbe055048734a83eaf1b8e6
blacklytning <blacklytning@yandex.com>
2022-08-02 13:59:17 +0530

    hanoip: Import rootdir from moto-common
    Signed-off-by: blacklytning <blacklytning@yandex.com>
    
    Co-authored-by: electimon <electimon@gmail.com>
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>

--- device.mk
+++ device.mk
@@ -34,13 +34,33 @@ PRODUCT_VENDOR_KERNEL_HEADERS := device/motorola/hanoip/prebuilt/kernel-headers
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/vendor/etc/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(DEVICE_PATH)/vendor/etc/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    $(DEVICE_PATH)/vendor/etc/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
+    $(DEVICE_PATH)/vendor/etc/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    $(DEVICE_PATH)/vendor/etc/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/vendor/etc/sysconfig/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
 # Device Init
 PRODUCT_PACKAGES += \
     fstab.hanoip \
     vendor-fstab.hanoip \
-    init.recovery.qcom.rc
+    init.recovery.qcom.rc \
+    init.class_main.sh \
+    init.mmi.charge_only.rc \
+    init.mmi.chipset.rc \
+    init.mmi.overlay.rc \
+    init.mmi.touch.sh \
+    init.mmi.usb.sh \
+    init.mmi.rc \
+    init.oem.fingerprint2.sh \
+    init.oem.hw.sh \
+    init.qcom.ipastart.sh \
+    init.qcom.rc \
+    init.qti.kernel.rc \
+    init.qti.kernel.sh \
+    init.target.rc \
+    vendor_modprobe.sh
 
 # AB Partitions
 AB_OTA_PARTITIONS += vendor_boot
@@ -57,6 +77,9 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.sf.lcd_density=400
 
+# Telephony: IMS framework
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/system/system_ext/etc/permissions/privapp-permissions-ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-ims.xml
 
 # HIDL
 PRODUCT_PACKAGES += \
@@ -66,6 +89,28 @@ PRODUCT_PACKAGES += \
 # Lights HAL
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.moto
+
+# QMI
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/vendor/etc/data/dsi_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/dsi_config.xml \
+    $(DEVICE_PATH)/vendor/etc/data/netmgr_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/netmgr_config.xml
+
+# QSEECOM TZ Storage
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/vendor/etc/gpfspath_oem_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gpfspath_oem_config.xml
+
+# Sec Configuration
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/vendor/etc/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/vendor/etc/seccomp_policy/imsrtp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/imsrtp.policy
+
+# Additional native libraries
+# See https://source.android.com/devices/tech/config/namespaces_libraries
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/vendor/etc/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/motorola/sm6150-common/platform.mk)
