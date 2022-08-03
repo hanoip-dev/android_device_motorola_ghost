4b9d934533a9b34f15c31d4cbc149e474c4ef247
electimon <electimon@gmail.com>
2022-08-03 08:08:23 +0530

    hanoip: Import Android.mk from sm6150-common
    Signed-off-by: blacklytning <blacklytning@yandex.com>

--- Android.mk
+++ Android.mk
@@ -4,4 +4,9 @@ LOCAL_PATH := $(call my-dir)
 
 include $(call all-subdir-makefiles)
 
+$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr: $(wildcard $(PRODUCT_VENDOR_KERNEL_HEADERS)/*)
+	rm -rf $@
+	mkdir -p $@/include
+	cp -a $(PRODUCT_VENDOR_KERNEL_HEADERS)/. $@/include
+
 endif
